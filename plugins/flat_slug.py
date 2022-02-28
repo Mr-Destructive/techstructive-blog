5a4b17a53a150b2427a8af5c47ffc92292a01e4f
Mr-Destructive <gormeet711@gmail.com>
2022-02-28 22:51:33 +0530

    fixed start links for feed

---
+++ plugins/flat_slug.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+from markata.hookspec import hook_impl
+
+if TYPE_CHECKING:
+    from markata import Markata
+
+
+@hook_impl(tryfirst=True)
+def pre_render(markata: "Markata") -> None:
+    for article in markata.iter_articles(description="creating slugs"):
+        try:
+            article["slug"] = article.metadata["slug"]
+        except KeyError:
+            article["slug"] = Path(article["path"])
